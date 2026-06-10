798ec7533f65d3f17131f1d419319eb74b19cc74
Sindre Moen <sindrenm@gmail.com>
2026-06-10 10:00:05 +0200

    fix(build): Compile against SDK 37

--- build-logic/plugins/src/main/kotlin/com/sats/johnnydeep/plugins/internal/ConfigureAndroidCommon.kt
+++ build-logic/plugins/src/main/kotlin/com/sats/johnnydeep/plugins/internal/ConfigureAndroidCommon.kt
@@ -3,7 +3,7 @@ package com.sats.johnnydeep.plugins.internal
 import com.android.build.api.dsl.CommonExtension
 
 internal fun CommonExtension.configureCommon() {
-  compileSdk = 36
+  compileSdk = 37
 
   defaultConfig.minSdk = 28
 }
